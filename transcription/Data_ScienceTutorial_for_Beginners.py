a196af89d26778078c9ef8754333755e433b9f8c
chameleon001 <chameleon001@naver.com>
2020-07-24 17:58:54 +0900

    no message

--- transcription/Data_ScienceTutorial_for_Beginners.py
+++ transcription/Data_ScienceTutorial_for_Beginners.py
@@ -19,5 +19,8 @@ Data Scientist는 아래의 기술들이 필요하다.
     2. Basic Statistics(기본 통계): 평균, 표준편차, 중간값. 기본 통계들을 알고 있다면 파이썬을 쉽게 사용할 수 있다.
     3. Data Munging(데이터 정제) : 정리가 되지 않은 데이터. 예를 들어 일치하지 않는 날짜, 문자열 형식 ex) 2020-01-01 , 01-02
                                 Python은 당신의 생각대로 정리가 되도록 도와준다.
-    4. Data Visualization : Data Visualization은 설명과 같다. 
+    4. Data Visualization(데이터 시각화) : Data Visualization은 설명과 같다. matplot, seaborn 같은 라이브러리를 이용하여 파이썬으로 데이터를 시각화한다.
+    5. Machine Learning(기계 학습) : Machine Learning 기술에 있는 수학을 이해할 필요는 없다. 기계학습의 기본을 이해하고 파이썬을 사용하여 구현하는 방법을 배우면 된다.
+
+    요약을 하자면 Data Scientist가 되는 파이썬을 배울 것이다.
 '''
